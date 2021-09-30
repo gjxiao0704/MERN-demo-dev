4863db9fa8b505305a39679482b1857864f95d90
sin255453 <sin255453@gmail.com>
2021-09-30 23:07:57 +0800

    add router

---
+++ frontend/src/APP2.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import TodoItem from './components/TodoItem'
+import AddTodo from './components/AddTodo'
+import { getTodos, addTodo, updateTodo, deleteTodo } from './API'
+import './App.css'
+
+const App2 = () => {
+  
+  return (
+   <h2>123456</h2>
+  )
+}
+
+export default App2
