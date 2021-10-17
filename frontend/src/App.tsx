318249abdc6e1b7f6b14951e2a3f32c4587451df
gjxiao <gjxiao0704@gmail.com>
2021-10-17 17:31:30 +0800

    add table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,22 @@ import AddTodo from './components/AddTodo'
 import { useAppSelector, useAppDispatch } from './app/hooks';
 import { getTodos, addTodo, updateTodo, deleteTodo } from './API'
 import './App.css'
-import { selectTodos,getTodosAsync } from './models/todosSlice'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { selectTodos, getTodosAsync } from './models/todosSlice'
+import { Toast } from 'primereact/toast';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import './index.css';
+import ReactDOM from 'react-dom';
+
 
 const App = () => {
   const todos = useAppSelector(selectTodos);
   const dispatch = useAppDispatch();
-  
+
   useEffect(() => {
     dispatch(getTodosAsync())
   }, []);
@@ -27,7 +37,7 @@ const App = () => {
       .then(_ => dispatch(getTodosAsync()))
       .catch(err => console.error(err))
   }
-
+  const [selectedProduct6, setSelectedProduct6] = useState(null);
   const handleDeleteTodo = (_id: string): void => {
     deleteTodo(_id)
       .then(() => dispatch(getTodosAsync()))
@@ -35,10 +45,10 @@ const App = () => {
   }
 
   return (
-    
+
     <main className="App">
       <h1>My Todos</h1>
-      
+
       <AddTodo saveTodo={handleSaveTodo} />
       {
         todos.map((todo: ITodo) => (
@@ -50,10 +60,19 @@ const App = () => {
           />
         ))
       }
-      
-  
+
+      <div className="datatable-selection-demo" >
+        <DataTable value={todos} selectionMode="radiobutton" selection={selectedProduct6} onSelectionChange={e => setSelectedProduct6(e.value)} dataKey="name">
+          <Column selectionMode="single" headerStyle={{ width: '3em' }}></Column>
+          <Column field="name" header="Name"></Column>
+          <Column field="description" header="description"></Column>
+        </DataTable>
+      </div>
+
+
+
     </main>
-    
+
   )
 }
 
